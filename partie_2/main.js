0d15ed7547d860213a5227552d01cadc5777c134
Antoine Verlyck <antoine.verlyck@gmail.com>
2022-12-15 16:13:03 +0100

    exo termine

--- partie_2/main.js
+++ partie_2/main.js
@@ -1,15 +1,30 @@
 const PRODUCT_LIST = document.querySelector(".product_list");
 const SHOPPING_BASKET = document.querySelector(".shopping_basket");
 let product_count_obj = { Pomme: 0, Poire: 0, Prune: 0 };
+let product_price_obj = { Pomme: 2, Poire: 3, Prune: 1 };
+let total = 0;
 
+/**
+ * Renvoie true si la chaine de caractère est présent dans l'un des enfant du noeud, renvoie false si non
+ * @param {string} string
+ * @param {nodeList} node_list
+ * @returns bool
+ */
 function is_string_in_NodeList(string, node_list) {
   let bool = false;
   node_list.forEach((element) => {
-    bool = element.textContent.includes(string);
+    if (element.textContent.includes(string)) {
+      bool = true;
+    }
   });
   return bool;
 }
 
+/**
+ * Supprime les derniers enfants jusqu'a arrivé a la limite de nombre d'enfants (max_length)
+ * @param {node} node
+ * @param {int} max_length
+ */
 function remove_child_from_NodeList(node, max_length) {
   while (node.childNodes.length > max_length) {
     node.removeChild(node.lastChild);
@@ -18,14 +33,14 @@ function remove_child_from_NodeList(node, max_length) {
 
 PRODUCT_LIST.childNodes.forEach((element_list) => {
   element_list.addEventListener("click", (e) => {
-    const PRODUCT = element_list.textContent;
+    const TOTAL_DIV = document.querySelector("#total");
+    const PRODUCT = element_list.textContent.split(" ")[0];
     const NEW_P = document.createElement("p");
 
     NEW_P.textContent = PRODUCT;
 
     if (!is_string_in_NodeList(PRODUCT, SHOPPING_BASKET.childNodes)) {
       SHOPPING_BASKET.appendChild(NEW_P);
-      product_count_obj[PRODUCT]++;
     }
 
     SHOPPING_BASKET.childNodes.forEach((element_basket) => {
@@ -39,17 +54,43 @@ PRODUCT_LIST.childNodes.forEach((element_list) => {
 
       remove_child_from_NodeList(element_basket, 3);
 
-      console.log(NEW_SPAN);
-
       NEW_SPAN.setAttribute("id", PRODUCT);
-      const SPAN_PRODUCT = document.querySelector("#" + PRODUCT);
-
-      for (const key in product_count_obj) {
-        if (key == PRODUCT) {
-          product_count_obj[key]++;
-          SPAN_PRODUCT.textContent = product_count_obj[key];
-        }
-      }
     });
+
+    for (let key in product_count_obj) {
+      if (key == PRODUCT) {
+        const SPAN_PRODUCT = document.querySelector("#" + PRODUCT);
+        product_count_obj[key]++;
+        SPAN_PRODUCT.textContent =
+          " " +
+          product_count_obj[key] +
+          " x " +
+          product_price_obj[key] +
+          " € " +
+          " = " +
+          product_count_obj[key] * product_price_obj[key] +
+          " ";
+      }
+    }
+
+    const DELETE_SPAN = document.querySelectorAll("span:nth-of-type(even)");
+
+    DELETE_SPAN.forEach((element) => {
+      element.addEventListener("click", (e) => {
+        element.parentNode.remove();
+        product_count_obj[element.parentNode.textContent.split(" ")[0]] = 0;
+        total =
+          product_count_obj["Pomme"] * product_price_obj["Pomme"] +
+          product_count_obj["Poire"] * product_price_obj["Poire"] +
+          product_count_obj["Prune"] * product_price_obj["Prune"];
+        TOTAL_DIV.textContent = "Total = " + total + " €";
+      });
+    });
+    total =
+      product_count_obj["Pomme"] * product_price_obj["Pomme"] +
+      product_count_obj["Poire"] * product_price_obj["Poire"] +
+      product_count_obj["Prune"] * product_price_obj["Prune"];
+
+    TOTAL_DIV.textContent = "Total = " + total + " €";
   });
 });
